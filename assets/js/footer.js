b4209915461088f357d949c99221cb4c2b60c533
Gem Portfolio <aydavidttg@gmail.com>
2025-06-05 04:44:07 -0700

    Add files via upload

---
+++ assets/js/footer.js
@@ -0,0 +1,68 @@
+const footer = document.querySelector(".footer");
+
+footer.innerHTML = `
+    <div class="container">
+        <div class="row pb-3">
+            <div class="col-lg-5 col-md-12 text">
+                <a class="fs-3 name" href="https://gemportfolio.github.io/affiliatedafisi">
+                    Affiliatedafisi
+                </a>
+                <p>
+                    I help busy business owners create top-notch content that help them gain visibility and profitability.
+                </p>
+            </div>
+
+            <div class="col-lg-2 col-md-6">
+                <h5>Links</h5>
+                <ul class="list-unstyled mb-3">
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#services">Our Services</a>
+                    </li>
+                    <li>
+                        <a href="#testimonials">Testimonials</a>
+                    </li>
+                    <li>
+                        <a href="#consultation">Book Consultation</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-lg-5 col-md-6 social">
+                <h5>Social Handles</h5>
+                <div class="">
+                    <a href="https://facebook.com/Affiliatedafisi/">
+                        <i class="bi bi-facebook fs-3 pe-3"></i>
+                    </a>
+                    <a href="https://www.instagram.com/officialafisiblessing/">
+                        <i class="bi bi-instagram fs-3 pe-3"></i>
+                    </a>
+                    <a href="https://x.com/blessingafisi/">
+                        <i class="bi bi-twitter-x fs-3"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr class="mx-auto" />
+    <div class="text-center copyright pt-2">
+        © Copyright 2025,
+        <a href="https://gemportfolio.github.io/affiliatedafisi" class="link"
+            >Affiliatedafisi</a
+        >
+    </div>
+    <div class="text-center copyright pb-3">
+        Designed by
+        <a href="https://thecreativeayd.com.ng/" class="link"
+            >TheCreativeAyd</a
+        >
+    </div>    
+    `;
+
+
+   
+       
+       
+       
---
+++ assets/js/navbar.js
@@ -0,0 +1,64 @@
+const nav = document.querySelector(".navbar");
+
+nav.innerHTML = `
+	<div class="container py-2">
+		<a class="navbar-brand" href="https://gemportfolio.github.io/affiliatedafisi">
+			<!--span><img src="./assets/img/logo.png"></span-->
+			Affiliatedafisi
+		</a>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarSupportedContent"
+			aria-controls="navbarSupportedContent"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav ms-0 ms-auto">
+				<li class="nav-item my-auto">
+					<a class="nav-link" href="./index.html">Home</a>
+				</li>
+				<li class="nav-item my-auto">
+					<a class="nav-link" href="#services">Our Services</a>
+				</li>
+				<li class="nav-item my-auto">
+					<a class="nav-link" href="#testimonials">Testimonials</a>
+				</li>
+				<li class="nav-item my-auto">
+					<a class="nav-link-btn lazy-el" href="#ebooks">
+						Buy My eBooks
+						<i class="bi bi-cart ps-1"></i>
+					</a>
+				</li>
+				<div class="d-flex">
+					<li class="nav-item ct my-auto">
+						<a class="nav-icon" href="https://facebook.com/Affiliatedafisi/">
+							<i class="bi bi-facebook"></i>
+						</a>
+					</li>
+					<li class="nav-item ct my-auto">
+						<a class="nav-icon" href="https://www.instagram.com/officialafisiblessing/">
+							<i class="bi bi-instagram"></i>
+						</a>
+					</li>
+					<li class="nav-item ct my-auto">
+						<a class="nav-icon" href="https://x.com/blessingafisi/">
+							<i class="bi bi-twitter-x"></i>
+						</a>
+					</li>
+				</div>
+			</ul>
+		</div>
+	</div>
+    `;
+
+
+   
+   
+   
+   
+   
